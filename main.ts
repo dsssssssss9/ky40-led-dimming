5b7476de361dbc0d82a01759d119b8c3d33fd228
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-02-08 20:51:49 +0000

    Restore '08/02/2022, 20:15:10 test rotate both directions to increase / decrease LED brightness'

--- main.ts
+++ main.ts
@@ -1,10 +1,8 @@
-KY040.onTurned(direction.clockwise, function () {
-    Act_B += B_Step * -1
-    if (Act_B <= Min_B) {
-        Act_B = Min_B
-        basic.showString("Min")
-    }
-    pins.analogWritePin(AnalogPin.P0, Act_B)
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
 })
 KY040.onTurned(direction.counterclockwise, function () {
     Act_B += B_Step
@@ -17,10 +15,9 @@ KY040.onTurned(direction.counterclockwise, function () {
 let B_Step = 0
 let Act_B = 0
 let Max_B = 0
-let Min_B = 0
-basic.showIcon(IconNames.Skull)
+basic.showIcon(IconNames.Pitchfork)
 KY040.setKY040(DigitalPin.P14, DigitalPin.P15)
-Min_B = 0
+let Min_B = 0
 Max_B = 1023
 Act_B = 0
 B_Step = 63
