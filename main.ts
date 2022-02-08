585402b1b2f6def743143a5d96b9b82f832c000a
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-02-08 20:59:40 +0000

    Restore '08/02/2022, 20:15:10 test rotate both directions to increase / decrease LED brightness'

--- main.ts
+++ main.ts
@@ -1,57 +1,8 @@
-/**
- * Use KY-40 Rotary Encoder to alter brightness of External LED
- * 
- * using analogue output of Micro:Bit - sort of simple PWM
- * 
- * -------------------------------------------------------------------------------------
- * 
- * Uses KY-40 Extension by M Klein...
- * 
- * https://github.com/MKleinSB/pxt-KY-040
- * 
- * ------------------------------------------------------------------
- * 
- * Led connected to PIN0 & GND via a 470Ω resistor to limit current
- * 
- * -----------------------------------------------------------------------------
- * 
- * KY-40 Encoder connected  as follows..... 
- * 
- * KY-40           Micro:Bit
- * 
- * -------------------------------
- * 
- * GND  ----->    GND
- * 
- * +        ----->    3v
- * 
- * CLK   ----->    PIN14
- * 
- * DT    ----->     PIN15
- * 
- * SW     *NOT USED*
- * 
- * ------------------------------------------------------
- * 
- * MIN_B    =   Minimum LED brightness
- * 
- * Max_B    =   Maximum LED Brightness
- * 
- * Act_B      =   Current displayed LED Brightness
- * 
- * B_Step    =   Amount to alter brightness per click of the Encoder
- * 
- * ---------------------------------------------------------------------------------------------
- * 
- * When Max / Min Brightness level is reached value will not go higher/lower & Micro:Bit Display informs user they have reached Max/Min level
- */
-KY040.onTurned(direction.clockwise, function () {
-    Act_B += B_Step * -1
-    if (Act_B <= Min_B) {
-        Act_B = Min_B
-        basic.showString("Min")
-    }
-    pins.analogWritePin(AnalogPin.P0, Act_B)
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
 })
 KY040.onTurned(direction.counterclockwise, function () {
     Act_B += B_Step
@@ -64,10 +15,9 @@ KY040.onTurned(direction.counterclockwise, function () {
 let B_Step = 0
 let Act_B = 0
 let Max_B = 0
-let Min_B = 0
-basic.showIcon(IconNames.Happy)
+basic.showIcon(IconNames.Pitchfork)
 KY040.setKY040(DigitalPin.P14, DigitalPin.P15)
-Min_B = 0
+let Min_B = 0
 Max_B = 1023
 Act_B = 0
 B_Step = 63
