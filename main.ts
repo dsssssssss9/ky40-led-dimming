c56a56843e47002735d177c551489d5aa9f5138a
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-02-08 19:42:56 +0000

    Preess A to increase LED brightness

--- main.ts
+++ main.ts
@@ -1,10 +1,22 @@
 input.onButtonPressed(Button.A, function () {
-    pins.analogWritePin(AnalogPin.P0, 1023)
+    Act_B += B_Step
+    if (Act_B >= Max_B) {
+        Act_B = Max_B
+    }
+    pins.analogWritePin(AnalogPin.P0, Act_B)
 })
 input.onButtonPressed(Button.B, function () {
     pins.analogWritePin(AnalogPin.P0, 256)
 })
+let B_Step = 0
+let Act_B = 0
+let Max_B = 0
 basic.showIcon(IconNames.No)
+let Min_B = 0
+Max_B = 1023
+Act_B = 0
+B_Step = 10
+pins.analogWritePin(AnalogPin.P0, Act_B)
 basic.forever(function () {
 	
 })
