c784bd07be64960811b520096dec648cc76e74cd
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-02-08 19:52:03 +0000

    change step to 63 & press B to dim LED

--- main.ts
+++ main.ts
@@ -6,17 +6,19 @@ input.onButtonPressed(Button.A, function () {
     pins.analogWritePin(AnalogPin.P0, Act_B)
 })
 input.onButtonPressed(Button.B, function () {
-    pins.analogWritePin(AnalogPin.P0, 256)
+    Act_B += B_Step * -1
+    if (Act_B <= Min_B) {
+        Act_B = Min_B
+    }
+    pins.analogWritePin(AnalogPin.P0, Act_B)
 })
 let B_Step = 0
 let Act_B = 0
 let Max_B = 0
-basic.showIcon(IconNames.No)
 let Min_B = 0
+basic.showIcon(IconNames.Giraffe)
+Min_B = 0
 Max_B = 1023
 Act_B = 0
-B_Step = 10
+B_Step = 63
 pins.analogWritePin(AnalogPin.P0, Act_B)
-basic.forever(function () {
-	
-})
