d97368bc0d08b4e32807a0ce7a86d8b045a081c5
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-02-08 20:46:06 +0000

    Restore '08/02/2022, 20:16:37 set decrease direction to correct value'

--- main.ts
+++ main.ts
@@ -1,50 +1,3 @@
-/**
- * Use KY-40 Rotary Encoder to alter brightness of External LED
- * 
- * using analogue output of Micro:Bit - sort of simple PWM
- * 
- * -------------------------------------------------------------------------------------
- * 
- * Uses KY-40 Extension by M Klein...
- * 
- * https://github.com/MKleinSB/pxt-KY-040
- * 
- * ------------------------------------------------------------------
- * 
- * Led connected to PIN0 & GND via a 470Ω resistor to limit current
- * 
- * -----------------------------------------------------------------------------
- * 
- * KY-40 Encoder connected  as follows..... 
- * 
- * KY-40           Micro:Bit
- * 
- * -------------------------------
- * 
- * GND  ----->    GND
- * 
- * +        ----->    3v
- * 
- * CLK   ----->    PIN14
- * 
- * DT    ----->     PIN15
- * 
- * SW     *NOT USED*
- * 
- * ------------------------------------------------------
- * 
- * MIN_B    =   Minimum LED brightness
- * 
- * Max_B    =   Maximum LED Brightness
- * 
- * Act_B      =   Current displayed LED Brightness
- * 
- * B_Step    =   Amount to alter brightness per click of the Encoder
- * 
- * ---------------------------------------------------------------------------------------------
- * 
- * When Max / Min Brightness level is reached value will not go higher/lower & Micro:Bit Display informs user they have reached Max/Min level
- */
 KY040.onTurned(direction.clockwise, function () {
     Act_B += B_Step * -1
     if (Act_B <= Min_B) {
@@ -65,7 +18,7 @@ let B_Step = 0
 let Act_B = 0
 let Max_B = 0
 let Min_B = 0
-basic.showIcon(IconNames.Happy)
+basic.showIcon(IconNames.Skull)
 KY040.setKY040(DigitalPin.P14, DigitalPin.P15)
 Min_B = 0
 Max_B = 1023
