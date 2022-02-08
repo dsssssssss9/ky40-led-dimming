bea418ceec6c4952d13c6640f3713039775da0c9
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-02-08 20:54:40 +0000

    Restore '08/02/2022, 19:52:03 change step to 63 & press B to dim LED'

--- main.ts
+++ main.ts
@@ -1,23 +1,23 @@
 input.onButtonPressed(Button.A, function () {
-	
-})
-input.onButtonPressed(Button.B, function () {
-	
-})
-KY040.onTurned(direction.counterclockwise, function () {
     Act_B += B_Step
     if (Act_B >= Max_B) {
         Act_B = Max_B
-        basic.showString("Max")
+    }
+    pins.analogWritePin(AnalogPin.P0, Act_B)
+})
+input.onButtonPressed(Button.B, function () {
+    Act_B += B_Step * -1
+    if (Act_B <= Min_B) {
+        Act_B = Min_B
     }
     pins.analogWritePin(AnalogPin.P0, Act_B)
 })
 let B_Step = 0
 let Act_B = 0
 let Max_B = 0
-basic.showIcon(IconNames.Pitchfork)
-KY040.setKY040(DigitalPin.P14, DigitalPin.P15)
 let Min_B = 0
+basic.showIcon(IconNames.Giraffe)
+Min_B = 0
 Max_B = 1023
 Act_B = 0
 B_Step = 63
